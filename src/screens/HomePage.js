de53c0643893630a52ff4cc210376d0e961884bf
PC <lsongulija@gmail.com>
2021-03-28 16:37:45 +0200

    Home page added. homePage.scsss styling added

---
+++ src/screens/HomePage.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import './homePage.scss'
+function HomePage() {
+    return (
+        <div className='homepage'>
+            <div className='directory-menu'>
+                <div className='menu-item'>
+                    <div className='content'>
+                        <h1 className='title'>Hats</h1>
+                        <span className='subtitle'>Shop now</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default HomePage
