857b4781a00da11abd591cff7d8e5260d95f81e6
songulija <65647664+songulija@users.noreply.github.com>
2021-03-29 00:18:09 +0300

    First routes added with help of react-router-dom. With use of Router, Switch, Link and Router that come from that module

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 
+import React from 'react'
+import {Route, Link, Switch} from 'react-router-dom'
 import './App.css';
 import HomePage from './screens/HomePage';
 
-function App() {
+function App() {//to use Routes in App componennt we use Router
   return (
     <div>
-      <HomePage />
-    </div>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+      </Switch>
+    </div>    
   );
-}
+}//when user goes to localhost:3000/ route then render HomePage component
 
 export default App;
