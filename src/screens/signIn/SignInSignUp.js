016b038954455d83ee6d17b540b8b126923979cd
PC <lsongulija@gmail.com>
2021-04-01 20:11:55 +0200

    Sign up with email and password added. it adds it to Authentication and also saves document in firestore

--- src/screens/signIn/SignInSignUp.js
+++ src/screens/signIn/SignInSignUp.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import SignIn from '../../components/signIn/SignIn'
+import SignUp from '../../components/signUp/SignUp'
+import './styles.scss'
 
 function SignInSignUp() {
     return (
         <div className='sign-in-and-sign-up'>
             <SignIn />
+            <SignUp />
         </div>
 
     )
