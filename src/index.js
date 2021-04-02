4bdba0dd6c4ffc0b1853fb9c48f7b0b87304f7ed
songulija <65647664+songulija@users.noreply.github.com>
2021-04-02 23:23:40 +0300

    Redux store added, userAction amd userReducer added

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from 'react-router-dom'
+import {Provider } from 'react-redux'
+import store from './redux/store.js'
 import './index.css';
 import App from './App';
 
-
-ReactDOM.render(<Router><App /></Router>,document.getElementById('root'));
+//we wrap everyting in Provider so whole app has access to Store(global state)
+//pass store to provider
+ReactDOM.render(
+    <Provider store={store}>
+    <Router>
+        <App />
+    </Router>
+    </Provider>
+,document.getElementById('root'));
 
